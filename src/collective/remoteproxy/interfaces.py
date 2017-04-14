fda3fb6e9d9d7adf35d3e302ba69fc5f52166745
Johannes Raggam <thetetet@gmail.com>
2017-04-14 13:09:24 +0200

    remote browser layer. not needed here, views might be easier to override without it.

--- src/collective/remoteproxy/interfaces.py
+++ src/collective/remoteproxy/interfaces.py
@@ -4,11 +4,6 @@
 from collective.remoteproxy import _
 from zope import schema
 from zope.interface import Interface
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-
-
-class IBrowserLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
 
 
 class IRemoteProxySchema(Interface):
