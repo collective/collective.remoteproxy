99acedd7c98617bd5182a8734bbfb7c8273ad31d
Johannes Raggam <thetetet@gmail.com>
2019-01-17 14:58:41 +0100

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.0'
+version = '1.1.dev0'
 short_description = "Proxy for remote content"
 long_description = "\n\n".join([
     open('README.rst').read(),
