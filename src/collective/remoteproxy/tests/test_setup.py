84029362fd35b2f6f5c58630f70ec9fb28f4c440
Jens W. Klein <jk@kleinundpartner.at>
2021-02-05 16:51:04 +0100

    fix tests

--- src/collective/remoteproxy/tests/test_setup.py
+++ src/collective/remoteproxy/tests/test_setup.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from collective.remoteproxy.testing import (  # noqa
-    COLLECTIVE_REMOTEPROXY_INTEGRATION_TESTING,
-)
 from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from ..testing import COLLECTIVE_REMOTEPROXY_INTEGRATION_TESTING  # noqa: E501
 
 import unittest
+
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
 
 
 class TestSetup(unittest.TestCase):
@@ -15,29 +22,34 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer["portal"]
-        self.installer = api.portal.get_tool("portal_quickinstaller")
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
 
     def test_product_installed(self):
         """Test if collective.remoteproxy is installed."""
         self.assertTrue(self.installer.isProductInstalled("collective.remoteproxy"))
 
-    def test_browserlayer(self):
-        """Test that IBrowserLayer is registered."""
-        from collective.remoteproxy.interfaces import IBrowserLayer
-        from plone.browserlayer import utils
-
-        self.assertIn(IBrowserLayer, utils.registered_layers())
-
 
 class TestUninstall(unittest.TestCase):
+    """Test that collective.remoteproxy is properly uninstalled."""
 
     layer = COLLECTIVE_REMOTEPROXY_INTEGRATION_TESTING
 
     def setUp(self):
         self.portal = self.layer["portal"]
-        self.installer = api.portal.get_tool("portal_quickinstaller")
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer["request"])
+        else:
+            self.installer = api.portal.get_tool("portal_quickinstaller")
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
         self.installer.uninstallProducts(["collective.remoteproxy"])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if collective.remoteproxy is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled("collective.remoteproxy"))
+        self.assertFalse(
+            self.installer.isProductInstalled("collective.remoteproxy")
+        )
